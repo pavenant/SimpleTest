e731eba932f7e3c7e7b2552e945705c476ac21c4
Mark.Riley <Mark.Riley@shelbyfinance.com>
2022-03-30 15:10:21 +0100

    Passed tests.

--- IntegrationTest/UnitTest1.cs
+++ IntegrationTest/UnitTest1.cs
@@ -1,39 +1,65 @@
 
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
 namespace IntegrationTest
 {
     [TestClass]
     public class UnitTest1
     {
+        private const string ExceptionMessage = "data not correct";
+
         [TestMethod]
-        public void WordSort()
+        public void CalculateTotal_SortsByWord()
         {        
             Assert.AreEqual("Boom Zoom", SimpleTest.MyTest.CalculateTotal("Zoom Boom"));
         }
 
         [TestMethod]
-        public void CaseSort()
+        public void CalculateTotal_SortsByCase()
         {        
             Assert.AreEqual("Boom boom", SimpleTest.MyTest.CalculateTotal("boom Boom"));
         }
 
-         [TestMethod]
-        public void RemoveInvalidChars()
+        [TestMethod]
+        public void CalculateTotal_RemovesInvalidCharacters()
         {        
-            Assert.AreEqual("b b", SimpleTest.MyTest.CalculateTotal("b, b"));
+            Assert.AreEqual("b b", SimpleTest.MyTest.CalculateTotal("b.,;' b"));
         }
 
         [TestMethod]
-        public void SimpleTest1()
+        public void CalculateTotal_ByWordCaseAndRemovesInvalidCharacters_SimpleTest1()
         {        
             Assert.AreEqual("baby Go go", SimpleTest.MyTest.CalculateTotal("Go baby, go"));
         }
 
         [TestMethod]
-        public void SimpleTest2()
+        public void CalculateTotal_ByWordCaseAndRemovesInvalidCharacters_SimpleTest2()
         {
             Assert.AreEqual("ABC aBc abc CBA CBA cba", SimpleTest.MyTest.CalculateTotal("CBA, abc aBc ABC cba CBA."));
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), ExceptionMessage)]
+        public void CalculateTotal_WhenInputIsNull_ThenRaiseNullException()
+        {
+            SimpleTest.MyTest.CalculateTotal(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), ExceptionMessage)]
+        public void CalculateTotal_WhenInputIsEmpty_ThenRaiseNullException()
+        {
+            SimpleTest.MyTest.CalculateTotal("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), ExceptionMessage)]
+        public void CalculateTotal_WhenInputIsSpace_ThenRaiseNullException()
+        {
+            SimpleTest.MyTest.CalculateTotal(" ");
+        }
+
     }
 }
--- SimpleTest/ThisIsATest.cs
+++ SimpleTest/ThisIsATest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 //simple test
@@ -16,21 +17,31 @@ namespace SimpleTest
         public static string CalculateTotal(string someInput)
         {
             var log = new ConsoleLogger();
-            if (someInput == null)
+
+            if (string.IsNullOrWhiteSpace(someInput))
             {
-                throw new DataMisalignedException("data not correct");
+                throw new ArgumentNullException("data not correct");
             }
             
             log.Log("start CalculateTotal");
 
             //algorithm
-            if (someInput == "Go baby, go")
-            {                
-                return "baby Go go";
-            }
+            var pattern = new Regex("[.,;']");
+            var filteredWords = pattern.Replace(someInput, "");
+
+            var words = filteredWords.Split(' ');
+
+            var orderedWords = words.OrderBy(x => x);
+
+            var upperToLowerCaseSort = orderedWords
+                .OrderBy(x => x, StringComparer.OrdinalIgnoreCase)
+                .ThenBy(x => x, StringComparer.Ordinal);
+
+            var orderedWordString = string.Join(" ", upperToLowerCaseSort);
 
             log.Log("end CalculateTotal");
-            return someInput;            
+
+            return orderedWordString;
         }
 
         internal class ConsoleLogger : ILogger
