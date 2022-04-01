9ceb48f242afd7b603b09737676d0f2031d16fc2
Nikola Radevic <radevic.nikola@gmail.com>
2022-04-01 17:31:33 +0200

    Nik Test Complete

--- SimpleTest/ThisIsATest.cs
+++ SimpleTest/ThisIsATest.cs
@@ -15,22 +15,59 @@ namespace SimpleTest
     {
         public static string CalculateTotal(string someInput)
         {
-            var log = new ConsoleLogger();
-            if (someInput == null)
-            {
-                throw new DataMisalignedException("data not correct");
-            }
+            var logger = new ConsoleLogger(); // new EventLogger();
             
-            log.Log("start CalculateTotal");
+            if (string.IsNullOrEmpty(someInput))
+            {
+                throw new Exception("data not correct");
+            }
+
+            Log(logger, "start CalculateTotal");
 
             //algorithm
-            if (someInput == "Go baby, go")
-            {                
-                return "baby Go go";
+            string cleanInput = CleanupChars(someInput);
+
+            var orderedEnum = cleanInput
+                                .Split(' ')
+                                .Select(s => s)
+                                .OrderBy(s => s, StringComparer.OrdinalIgnoreCase)
+                                .ThenBy(s => s, StringComparer.Ordinal);
+
+            string result = String.Join(" ", orderedEnum);
+
+            Log(logger, "end CalculateTotal");
+
+            return result;
+        }
+
+        // private for same accessibility as ILogger
+        private static string CalculateTotalInjectedLogger(string someInput, ILogger logger)
+        {            
+            if (string.IsNullOrEmpty(someInput))
+            {
+                throw new Exception("data not correct");
             }
 
-            log.Log("end CalculateTotal");
-            return someInput;            
+            logger.Log("end CalculateTotal");
+
+            //algorithm
+            string cleanInput = CleanupChars(someInput);
+
+            var orderedEnum = cleanInput
+                                .Split(' ')
+                                .Select(s => s)
+                                .OrderBy(s => s, StringComparer.OrdinalIgnoreCase)
+                                .ThenBy(s => s, StringComparer.Ordinal);
+
+            string result = String.Join(" ", orderedEnum);
+
+            logger.Log("end CalculateTotal");
+            return result;
+        }
+
+        private static string CleanupChars(string s)
+        {
+            return s.Replace(".", "").Replace(",", "").Replace(";", "").Replace("'", "");
         }
 
         internal class ConsoleLogger : ILogger
@@ -41,5 +78,22 @@ namespace SimpleTest
             }
         }
 
+        internal class EventLogger : ILogger {
+            public void Log(string stuff)
+            {
+                // TODO EventLog Accordingly
+                throw new NotImplementedException();
+            }
+        }
+
+        // Method Overloads version
+        private static void Log(ConsoleLogger logger, string message)
+        {
+            logger.Log(message);
+        }
+        private static void Log(EventLogger logger, string message)
+        {
+            logger.Log(message);
+        }
     }
 }
