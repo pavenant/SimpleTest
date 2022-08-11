3a2033868afcd8b04bc92a3aec0786167b349d76
Graham Griffiths <grahamrgriffiths@gmail.com>
2022-08-11 11:39:27 +0100

    CP-TABCS-2 Rename / Restructure Methods
    Rename implementation and start to adjust approach

--- Core/SortingLogic/TextABCSort.cs
+++ Core/SortingLogic/TextABCSort.cs
@@ -3,6 +3,9 @@ using Core.Interfaces;
 
 namespace Core.SortingLogic
 {
+    /// <summary>
+    /// TextABCSort
+    /// </summary>
     public class TextABCSort : IStringSorter
     {
         private readonly ILogger _logger;
@@ -11,23 +14,30 @@ namespace Core.SortingLogic
             _logger = logger;
         }
 
-        public string SortString(string someInput)
+        /// <summary>
+        /// Sorting Implementation to be used to aid reading though the words 
+        /// alphabetically and easily see all the variants of different case
+        /// </summary>
+        /// <param name="stringInput"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public string SortString(string stringInput)
         {
-            if (someInput == null)
+            if (stringInput == null)
             {
-                throw new DataMisalignedException("data not correct");
+                throw new ArgumentNullException(nameof(stringInput), "String input is required");
             }
             
             _logger.Log("start ABC Sort");
 
-            //algorithm
-            if (someInput == "Go baby, go")
-            {                
-                return "baby Go go";
-            }
+            //TODO: sorting algorithm
+            // words should be reordered Alphabetically - (Zerbra Abba) becomes (Abba Zebra)
+            // words should THEN be ordered from upper case to lower case. Note point 1 takes preference. (aBba Abba) becomes (Abba aBba)
+            // remove all (.,;') chars. (aBba, Abba) becomes (Abba aBba)
+            // Do not remove duplicate words
 
             _logger.Log("end ABC Sort");
-            return someInput;            
+            return stringInput;            
         }
     }
 }
